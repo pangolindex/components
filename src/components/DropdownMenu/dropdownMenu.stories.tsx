bfcfb4b7ce9a00b45f3d6ef3b748a7f1752ef97d
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2022-09-26 17:14:32 +0100

    Updated DropdownMenu story

--- src/components/DropdownMenu/dropdownMenu.stories.tsx
+++ src/components/DropdownMenu/dropdownMenu.stories.tsx
@@ -10,6 +10,63 @@ export default {
   args: {
     activeMenu: 'open',
   },
+  parameters: {
+    docs: {
+      description: {
+        component: 'Dropdown Menu Component',
+      },
+    },
+  },
+  argTypes: {
+    options: {
+      name: 'Options',
+      control: 'array',
+      type: { name: 'array', required: true },
+      description: 'Array of options that populate the select menu',
+    },
+    menuPlacement: {
+      name: 'Menu Placement',
+      control: 'select',
+      options: ['bottom', 'top', 'auto'],
+      type: { name: 'string', required: true },
+      defaultValue: 'auto',
+      description: 'Default placement of the menu in relation to the control.',
+    },
+    isMulti: {
+      name: 'IsMulti',
+      control: 'boolean',
+      type: { name: 'boolean', required: false },
+      defaultValue: false,
+      description: 'Allows the user to select multiple options',
+    },
+    placeHolder: {
+      name: 'PlaceHolder',
+      control: 'text',
+      type: { name: 'string', required: false },
+      defaultValue: 'Select',
+      description: 'Placeholder text for the dropdown component',
+    },
+    height: {
+      name: 'Height',
+      control: 'text',
+      type: { name: 'string', required: false },
+      defaultValue: '54px',
+      description: 'Height of the dropdown component',
+    },
+    onSelect: {
+      name: 'OnSelect',
+      control: 'function',
+      type: { name: 'function', required: false },
+      default: () => {},
+      description: 'Function that is called when an option is selected',
+    },
+    defaultValue: {
+      name: 'Default Value',
+      control: 'text',
+      type: { name: 'string', required: false },
+      description: 'Default value of the dropdown component',
+    },
+  },
 };
 
 const TemplateSimpleDropdown: ComponentStory<typeof DropdownMenu> = (args: any) => {
