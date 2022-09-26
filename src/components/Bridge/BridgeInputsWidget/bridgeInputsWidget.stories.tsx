97ba404e7ade9b01716406fdcdc264453616348e
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2022-09-26 16:02:41 +0100

    Updated Bridge Input Widget story

--- src/components/Bridge/BridgeInputsWidget/bridgeInputsWidget.stories.tsx
+++ src/components/Bridge/BridgeInputsWidget/bridgeInputsWidget.stories.tsx
@@ -13,6 +13,34 @@ export default {
       },
     },
   },
+  argTypes: {
+    title: {
+      name: 'Title',
+      control: 'text',
+      type: { name: 'string', required: true },
+      description: 'Title of the widget',
+    },
+    inputDisabled: {
+      name: 'Input Disabled',
+      control: 'boolean',
+      type: { name: 'boolean', required: true },
+      description: 'Disable the input',
+    },
+    isTokenDrawerOpen: {
+      name: 'Token Drawer Status',
+      control: 'boolean',
+      type: { name: 'boolean', required: false },
+      defaultValue: false,
+      description: 'Token drawer Status',
+    },
+    onTokenDrawerOpen: {
+      name: 'Token Drawer Open',
+      control: 'function',
+      type: { name: 'function', required: false },
+      defaultValue: () => {},
+      description: 'The function to be called when the token button is clicked',
+    },
+  },
 };
 
 const TemplateBridgeInputsWidget: ComponentStory<typeof BridgeInputsWidget> = (args: any) => {
