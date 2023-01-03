dfa606e06816f64d16bae561d573805d190563b3
Sarju <sarju.hansaliya@gmail.com>
2023-01-03 11:10:19 +0530

    fix: setup example (#188)
    * fix: setup example
    
    * chore: update eslint ignore
    
    * fix: example vite node polyfill issue
    
    * fix: move js file to ts file & update @pangolindex/web3-react-injected-connector version 1.0.2
    
    * fix: some scripts
    
    * fix: use src instead of lib folder
    
    * fix: usechainid hook logic
    
    * fix(example): env issue
    
    * fix: pool full ui component
    
    * fix: some codesmell
    
    * fix: theme changes in example
    
    * fix: example routing for  diffrent components
    
    * fix: wallet modal scroll issue
    
    * fix: readme and index route

---
+++ example/src/pages/Bridge/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Bridge } from '@components/index';
+
+function BridgeUI() {
+  return (
+    <div>
+      <Bridge />
+    </div>
+  );
+}
+
+export default BridgeUI;
