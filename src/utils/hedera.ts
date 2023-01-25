68d1b1bc8ac5e2cc12d4d82ea74f9ed6da7efbdb
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2023-01-25 11:59:25 +0530

    fix: client for mainnet

--- src/utils/hedera.ts
+++ src/utils/hedera.ts
@@ -293,7 +293,7 @@ class Hedera {
 
   constructor() {
     this.axios = BaseAxios.create({ timeout: 60000 });
-    this.client = Client.forTestnet(); // TODO check here for testnet and mainnet
+    this.client = Client.forMainnet(); // TODO check here for testnet and mainnet
   }
 
   async call<T>(config: AxiosRequestConfig) {
