5d1a7a0325552e27d3f59f8780c3352487324835
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2023-03-02 19:40:25 +0530

    fix: move native currency logo in tokens

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -1,13 +1,5 @@
-import { CAVAX, ChainId, Currency, Token } from '@pangolindex/sdk';
-import deepEqual from 'deep-equal';
+import { Currency, Token } from '@pangolindex/sdk';
 import React, { useMemo } from 'react';
-import EvmosLogo from 'src/assets/images/evmos.svg';
-import FlareLogo from 'src/assets/images/flare.svg';
-import HederaLogo from 'src/assets/images/hedera.svg';
-import NearLogo from 'src/assets/images/near.svg';
-import SongBirdLogo from 'src/assets/images/songbird.png';
-import WgmiLogo from 'src/assets/images/wagmi.png';
-import { AvaxLogo, CflrLogo } from 'src/components/Icons';
 import { LogoSize } from 'src/constants';
 import { useChainId } from 'src/hooks';
 import { getTokenLogoURL } from 'src/utils/getTokenLogoURL';
@@ -29,48 +21,11 @@ export default function CurrencyLogo({
   const chainId = useChainId();
 
   const srcs: string[] = useMemo(() => {
-    if (
-      currency === CAVAX[ChainId.AVALANCHE] ||
-      currency === CAVAX[ChainId.WAGMI] ||
-      currency === CAVAX[ChainId.COSTON] ||
-      currency === CAVAX[ChainId.SONGBIRD] ||
-      currency === CAVAX[ChainId.FLARE_MAINNET] ||
-      currency === CAVAX[ChainId.HEDERA_TESTNET] ||
-      currency === CAVAX[ChainId.HEDERA_MAINNET] ||
-      currency === CAVAX[ChainId.NEAR_TESTNET] ||
-      currency === CAVAX[ChainId.NEAR_MAINNET] ||
-      currency === CAVAX[ChainId.COSTON2] ||
-      currency === CAVAX[ChainId.EVMOS_TESTNET || currency === CAVAX[ChainId.EVMOS_MAINNET]]
-    )
-      return [];
-    if (currency instanceof Token || !!(currency as Token).address) {
-      const primarySrc = getTokenLogoURL((currency as Token)?.address, chainId, imageSize);
+    const primarySrc = getTokenLogoURL((currency as Token)?.address, chainId, imageSize);
 
-      return [primarySrc];
-    }
-
-    return [];
+    return [primarySrc];
   }, [currency]);
 
-  if (deepEqual(currency, CAVAX[ChainId.AVALANCHE])) {
-    return <AvaxLogo size={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.WAGMI])) {
-    return <img src={WgmiLogo} width={`${size}px`} height={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.COSTON])) {
-    return <CflrLogo size={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.NEAR_TESTNET]) || deepEqual(currency, CAVAX[ChainId.NEAR_MAINNET])) {
-    return <img src={NearLogo} width={`${size}px`} height={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.SONGBIRD])) {
-    return <img src={SongBirdLogo} width={`${size}px`} height={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.FLARE_MAINNET])) {
-    return <img src={FlareLogo} width={`${size}px`} height={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.HEDERA_TESTNET]) || deepEqual(currency, CAVAX[ChainId.HEDERA_MAINNET])) {
-    return <img src={HederaLogo} width={`${size}px`} height={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.COSTON2])) {
-    return <CflrLogo size={`${size}px`} className={className} />;
-  } else if (deepEqual(currency, CAVAX[ChainId.EVMOS_TESTNET]) || deepEqual(currency, CAVAX[ChainId.EVMOS_MAINNET])) {
-    return <img src={EvmosLogo} width={`${size}px`} height={`${size}px`} className={className} />;
-  }
   return (
     <StyledLogo
       className={className}
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,5 +1,3 @@
 export { default as Loading } from './Loading';
 export { default as LoaderIcon } from './LoaderIcon';
 export { default as LogoIcon } from './LogoIcon';
-export { default as AvaxLogo } from './AvaxLogo';
-export { default as CflrLogo } from './CflrLogo';
