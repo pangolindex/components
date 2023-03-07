7fb31e5ae6a8babe5622f43eb8cd108aa82a0cd9
Pedro <pedroantonioppms@gmail.com>
2023-03-07 13:38:29 -0400

    Revert "fix: remove liquidity field with same value"
    This reverts commit 73e480a2bc5d074db9e56363d3b214abcf14aa7b.

--- src/state/pburn/reducer.ts
+++ src/state/pburn/reducer.ts
@@ -6,14 +6,8 @@ export interface BurnState {
     readonly independentField: Field;
     readonly typedValue: string;
   };
-  [x: string]: {
-    readonly independentField: Field;
-    readonly typedValue: string;
-  };
 }
 
-export const initialKeyState = {
-  independentField: Field.CURRENCY_A,
 export const initialKeyState = {
   independentField: Field.CURRENCY_A,
   typedValue: '0',
@@ -21,16 +15,19 @@ export const initialKeyState = {
 
 const initialState: BurnState = {};
 
-const initialState: BurnState = {};
-
 export default createReducer<BurnState>(initialState, (builder) =>
-  builder.addCase(typeInput, (state, { payload: { pairAddress, field, typedValue } }) => {
-    const pairState = state[pairAddress] ? { ...state[pairAddress] } : initialKeyState;
-    state[pairAddress] = {
-      ...pairState,
-      independentField: field,
-      typedValue,
-    };
-    return state;
-  }),
+  builder
+    .addCase(typeInput, (state, { payload: { pairAddress, field, typedValue } }) => {
+      const pairState = state[pairAddress] ? { ...state[pairAddress] } : initialKeyState;
+      state[pairAddress] = {
+        ...pairState,
+        independentField: field,
+        typedValue,
+      };
+      return state;
+    })
+    .addCase(resetBurnState, (state, { payload: { pairAddress } }) => {
+      state[pairAddress] = initialKeyState;
+      return state;
+    }),
 );
