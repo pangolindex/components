10fbc756b1101f0f766631136182ed9e9c697b30
Bilal Corbacioglu <bilalcorbacioglu@users.noreply.github.com>
2023-03-08 17:33:33 +0300

    feat: concentrated liquidity UI - 1 (#293)
    * feat: concentrated liq added
    
    * feat: added storybooks & tr/table cleaned
    
    * feat: i18n key changes
    
    * fix: storybook view

---
+++ src/components/ConcentratedLiquidity/PositionCard/styles.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { Box, Text } from 'src/components';
+
+export const Card = styled.div`
+  background-color: ${({ theme }) => theme.concentratedLiquidity?.primaryBgColor};
+  border-radius: 10px;
+  padding: 32px;
+  display: flex;
+  flex-direction: row;
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+    justify-content: space-between;
+    width: max-content;
+  `};
+`;
+
+export const Row = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const BlackBox = styled(Box)`
+  background-color: ${({ theme }) => theme.concentratedLiquidity?.secondaryBgColor};
+  border-radius: 7px;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const BlackBoxContent = styled(Text)`
+  padding: 0.1rem 1rem;
+`;
+
+export const Data = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  padding-right: 30px;
+`;
+
+export const Price = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
---
+++ src/components/ConcentratedLiquidity/styles.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { Box } from 'src/components';
+
+export const PageWrapper = styled(Box)`
+  width: 100%;
+  height: calc(100vh - 76px);
+  padding-bottom: 10px;
+`;
+
+export const GridContainer = styled(Box)`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-gap: 12px;
+  padding: 50px 0px 0px;
+  height: 100%;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    padding: 10px 0px 0px;
+  `};
+`;
+
+export const Cards = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  overflow-y: auto;
+  white-space: nowrap;
+  width: 100%;
+`;
