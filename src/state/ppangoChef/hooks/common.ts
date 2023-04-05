b269e9f5b82bf221f7acd2f7b1526c2f1cd65c1a
Pedro <pedroantonioppms@gmail.com>
2023-04-05 17:40:58 -0400

    fix: sortby in farms version 3

--- src/state/ppangoChef/hooks/common.ts
+++ src/state/ppangoChef/hooks/common.ts
@@ -4,6 +4,7 @@ import { useMemo } from 'react';
 import { PNG } from 'src/constants/tokens';
 import { useChainId, usePangolinWeb3 } from 'src/hooks';
 import { useTokensCurrencyPriceHook } from 'src/hooks/multiChainsHooks';
+import { useTokensHook } from 'src/hooks/tokens';
 import { usePangoChefContract } from 'src/hooks/useContract';
 import { useSingleContractMultipleData } from '../../pmulticall/hooks';
 import { PangoChefInfo } from '../types';
@@ -13,8 +14,8 @@ import { usePangoChefInfosHook } from './index';
  * This hook returns the extra value provided by super farm extra reward tokens
  * @param rewardTokens array os tokens
  * @param rewardRate reward rate in png/s
- * @param multipliers multipler fro each token provider in rewardTokens
  * @param balance valueVariables from contract
+ * @param stakingInfo object containing the information of this farm
  * @returns return the extra percentage of apr provided by super farm extra reward tokens
  */
 export function usePangoChefExtraFarmApr(
@@ -81,6 +82,97 @@ export function usePangoChefExtraFarmApr(
 
     return extraAPR;
   }, [rewardTokens, multipliers, balance, pairPrice, tokensPrices, _rewardTokens, rewardRate]);
+}
+
+/**
+ * This hook returns a mapping, where key is the pid and the value is the farm's extra apr
+ * @param stakingInfos array of stakingInfo
+ * @returns return the mapping, key is pid, values of key is the extra apr, `{ 0: 10, 1: 0, 2: 5, ...}`
+ */
+export function usePangoChefUserExtraFarmsApr(stakingInfos: PangoChefInfo[]) {
+  const chainId = useChainId();
+  const useTokensCurrencyPrice = useTokensCurrencyPriceHook[chainId];
+  const useTokens = useTokensHook[chainId];
+
+  const rewardTokensAddresses = useMemo(() => {
+    const tokensMapping: { [x: string]: string } = {};
+    stakingInfos.forEach(({ rewardTokensAddress }) => {
+      rewardTokensAddress?.forEach((address) => {
+        tokensMapping[address] = address;
+      });
+    });
+    return Object.values(tokensMapping);
+  }, [stakingInfos]);
+
+  const _rewardTokens = useTokens(rewardTokensAddresses);
+
+  const rewardTokens = useMemo(() => {
+    if (_rewardTokens) {
+      return _rewardTokens.filter((token) => !!token) as Token[];
+    }
+    return [] as Token[];
+  }, [_rewardTokens]);
+
+  const tokensPrices = useTokensCurrencyPrice(rewardTokens);
+
+  const png = PNG[chainId];
+
+  // we need to divide by png.decimals
+  const aprDenominator = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(png.decimals));
+
+  return useMemo(() => {
+    const extraAPRs: { [x: string]: number | undefined } = {};
+
+    if (!rewardTokens || rewardTokens.length === 0) {
+      return extraAPRs;
+    }
+
+    for (let i = 0; i < stakingInfos.length; i++) {
+      let extraAPR = 0;
+      const stakingInfo = stakingInfos[i];
+
+      const pid = stakingInfo.pid;
+      const multipliers = stakingInfo.rewardTokensMultiplier;
+      const rewardsTokensAddresses = stakingInfo.rewardTokensAddress;
+      const pairPrice: Fraction | undefined = stakingInfo.pairPrice;
+      const balance = stakingInfo.stakedAmount;
+      const rewardRate = stakingInfo.userRewardRate;
+
+      if (!multipliers || multipliers.length === 0 || !rewardsTokensAddresses || rewardsTokensAddresses.length === 0) {
+        continue;
+      }
+
+      for (let index = 0; index < rewardsTokensAddresses.length; index++) {
+        const tokenAddress = rewardsTokensAddresses[index];
+        const token = rewardTokens.find((token) => token.address.toLowerCase() === tokenAddress.toLowerCase());
+        const tokenPrice = tokensPrices[tokenAddress];
+
+        const multiplier = multipliers[index];
+        if (!tokenPrice || !multiplier || !pairPrice || !token) {
+          extraAPRs[pid] = 0;
+          continue;
+        }
+
+        const pairBalance = pairPrice.multiply(balance);
+
+        //extraAPR = poolRewardRate(POOL_ID) * rewardMultiplier / (10** REWARD_TOKEN_PRICE.decimals) * 365 days * 100 * REWARD_TOKEN_PRICE / (pools(POOL_ID).valueVariables.balance * STAKING_TOKEN_PRICE)
+        extraAPR =
+          !tokenPrice || pairBalance.equalTo('0')
+            ? 0
+            : Number(
+                tokenPrice.raw
+                  .multiply(rewardRate)
+                  .multiply((365 * 86400 * 100).toString())
+                  .multiply(multiplier)
+                  .divide(pairBalance.multiply(aprDenominator))
+                  .divide((10 ** token.decimals).toString())
+                  .toSignificant(2),
+              );
+      }
+      extraAPRs[pid] = extraAPR;
+    }
+    return extraAPRs;
+  }, [rewardTokens, tokensPrices, stakingInfos]);
 }
 
 /**
