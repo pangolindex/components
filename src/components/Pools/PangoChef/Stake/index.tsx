662e00afbfd946169e68a3ab7cc5ebae8b6cc8fd
Pedro <pedroantonioppms@gmail.com>
2023-05-24 16:15:15 -0400

    fix: decimal issue in stake lp into farm

--- src/components/Pools/PangoChef/Stake/index.tsx
+++ src/components/Pools/PangoChef/Stake/index.tsx
@@ -121,10 +121,10 @@ const Stake = ({ onComplete, type, stakingInfo, combinedApr }: StakeProps) => {
     if (value === 100) {
       setTypedValue((userLiquidityUnstaked as TokenAmount).toExact());
     } else {
-      const newAmount = (userLiquidityUnstaked as TokenAmount)
-        .multiply(JSBI.BigInt(value))
-        .divide(JSBI.BigInt(100)) as TokenAmount;
-      setTypedValue(newAmount?.toFixed(0));
+      const lpToken = userLiquidityUnstaked.token;
+      const newAmount = userLiquidityUnstaked.multiply(JSBI.BigInt(value)).divide(JSBI.BigInt(100)) as TokenAmount;
+      console.log({ decimals: lpToken.decimals });
+      setTypedValue(newAmount?.toFixed(lpToken.decimals));
     }
   };
 
