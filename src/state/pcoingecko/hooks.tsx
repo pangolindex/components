b5c1c9439a644b5e9c3cb9399214ffb8b8151967
Pedro <pedroantonioppms@gmail.com>
2023-05-30 14:31:25 -0400

    fix: watchlist search

--- src/state/pcoingecko/hooks.tsx
+++ src/state/pcoingecko/hooks.tsx
@@ -2,7 +2,7 @@
 import { CHAINS, ChainId, Currency, Token } from '@pangolindex/sdk';
 import axios, { AxiosResponse } from 'axios';
 import { useEffect, useMemo, useState } from 'react';
-import { UseQueryResult, useQueries, useQuery } from 'react-query';
+import { useQueries, useQuery } from 'react-query';
 import { COINGECKO_CURRENCY_ID, COINGECKO_TOKENS_MAPPING, COINGEKO_BASE_URL } from 'src/constants/coingecko';
 import { useChainId } from 'src/hooks';
 import { CoingeckoWatchListState, useCoingeckoWatchList } from './atom';
@@ -293,11 +293,12 @@ export function useCoinGeckoTokens(): CoingeckoWatchListState {
   const { currencies, addCoingeckoTokens } = useCoingeckoWatchList();
 
   const queryParameter = [] as any;
-  const totalPages = 2;
+  const totalPages = 8;
   for (let page = 1; page <= totalPages; page++) {
     queryParameter.push({
       queryKey: ['get-coingecko-token-data', page],
       queryFn: fetchCoinMarketData(page),
+      staleTime: 1000 * 60 * 20, // 20 minutes
     });
   }
 
@@ -322,43 +323,5 @@ export function useCoinGeckoTokens(): CoingeckoWatchListState {
   }, [Object.values(apiTokens || []).length]);
 
   return currencies;
-}
-/**
- * its use for search token based on text
- * @param coinText
- * @returns [id: string]: CoingeckoWatchListToken
- */
-export function useCoinGeckoSearchTokens(coinText: string): {
-  [id: string]: CoingeckoWatchListToken;
-} {
-  const { data: searchTokens, isLoading } = useQuery<Array<CoingeckoTokenData>>(
-    ['coingeckoSearchTokens', coinText],
-    async () => {
-      const response: AxiosResponse = await coingeckoAPI.get(`search?query=${coinText}`);
-      return response?.data?.coins || [];
-    },
-    {
-      enabled: coinText.length > 0,
-    },
-  );
-
-  const coinIds = ((!isLoading && searchTokens) || []).map((item) => item.id);
-  const page = 1;
-
-  const results: UseQueryResult = useQuery(
-    ['get-coingecko-token-data', page, coinIds.join(',')],
-    fetchCoinMarketData(page, coinIds.join(',')),
-    {
-      enabled: coinIds?.length > 0,
-    },
-  );
-
-  const apiTokens = useMemo(() => {
-    const toknesData = results?.data as MarketCoinsAPIResponse[];
-
-    return makeCoingeckoTokenData(toknesData);
-  }, [results]);
-
-  return apiTokens;
 }
 /* eslint-enable max-lines */
