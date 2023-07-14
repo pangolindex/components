87ded4ed783a488c116759457dd089d159092971
Pedro <pedroantonioppms@gmail.com>
2023-07-14 13:28:39 -0400

    fix: overflow in sar stake error message

--- src/components/SarManageWidget/ConfirmDrawer/styled.ts
+++ src/components/SarManageWidget/ConfirmDrawer/styled.ts
@@ -10,9 +10,11 @@ export const Root = styled(Box)`
 `;
 
 export const ErrorWrapper = styled(Box)`
-  display: grid;
-  grid-template-rows: auto max-content;
-  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 100%;
 `;
 
 export const ErrorBox = styled(Box)`
@@ -20,6 +22,19 @@ export const ErrorBox = styled(Box)`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  flex: 1;
+  width: 100%;
+  flex-wrap: wrap;
+  word-wrap: break-word;
+`;
+
+export const ErrorText = styled(Text)`
+  font-weight: 500;
+  font-size: 16px;
+  color: ${({ theme }) => theme.red1};
+  text-align: center;
+  word-wrap: break-word;
+  width: 100%;
 `;
 
 export const SubmittedWrapper = styled(Box)`
