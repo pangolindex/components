fae1ff290aaa1082d598c1cf21ed2a027389dba5
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2023-09-13 22:53:11 +0530

    fix: WIP storybook changes

--- monorepo/packages/sar/.storybook/preview.tsx
+++ monorepo/packages/sar/.storybook/preview.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { Preview } from '@storybook/react';
-import { ThemeProvider } from '@honeycomb-finance/shared';
+import { useWeb3React as useWeb3ReactCore, createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core';
+import { ThemeProvider, PangolinWeb3Provider, NetworkContextName, getLibrary } from '@honeycomb-finance/shared';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { injected } from '@honeycomb-finance/wallet-connectors';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 1000 * 60,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
 
 const InternalProvider = ({ children, theme }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const context = useWeb3ReactCore();
+  const { library, chainId, account, activate } = context;
+
+  useEffect(() => {
+    // try to connect on page reload
+    if (activate) {
+      activate(injected);
+    }
+  }, []);
+
+  return (
+    <PangolinWeb3Provider chainId={chainId} library={library} account={account} key={chainId}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </PangolinWeb3Provider>
+  );
 };
 
 export const decorators = [
   (Story, metadata) => {
     const theme = metadata.args.theme ?? undefined;
     return (
-      <InternalProvider theme={theme}>
-        <Story />
-      </InternalProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Web3ProviderNetwork getLibrary={getLibrary}>
+          <InternalProvider theme={theme}>
+            <Story />
+          </InternalProvider>
+        </Web3ProviderNetwork>
+      </Web3ReactProvider>
     );
   },
 ];
